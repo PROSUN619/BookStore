18b0fcc0e8effbf752346c2eaaaad3d8a3634a0e
PROSUN619 <prosun.chakraborty92@gmail.com>
2021-12-04 23:12:34 +0530

    chapter 11 set up mvc

--- Webgentle.Bookstore/Webgentle.Bookstore/Startup.cs
+++ Webgentle.Bookstore/Webgentle.Bookstore/Startup.cs
@@ -16,6 +16,7 @@ namespace Webgentle.Bookstore
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddControllersWithViews();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,13 @@ namespace Webgentle.Bookstore
         app.UseDeveloperExceptionPage();
       }
 
+      
       app.UseRouting();
 
       //this is a end point
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapGet("/", async context =>
-              {
-            await context.Response.WriteAsync("Hello World!");
-          });
+        endpoints.MapDefaultControllerRoute();
       });
     }
   }
