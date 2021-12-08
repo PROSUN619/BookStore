f4d0d925f02d8e65fcf8bce89b6b9235523efc79
PROSUN619 <prosun.chakraborty92@gmail.com>
2021-12-08 11:26:41 +0530

    part 14 add views

---
+++ Webgentle.Bookstore/Webgentle.Bookstore/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.Bookstore.Models;
+using Webgentle.Bookstore.Repository;
+
+namespace Webgentle.Bookstore.Controllers
+{
+  public class BookController : Controller
+  {
+
+    private readonly BookRepository _bookModel;
+
+    public BookController()
+    {
+      _bookModel = new BookRepository();
+    }
+
+    public List<BookModel> GetAllBook()
+    {
+      return _bookModel.GetAllBooks();
+    }
+
+    public BookModel GetBook(int id)
+    {
+      return _bookModel.GetBook(id);
+    }
+
+    public List<BookModel> SearchBook(string bookname, string authorname )
+    {
+      return _bookModel.SearchBook(bookname, authorname);
+    }
+
+  }
+}
---
+++ Webgentle.Bookstore/Webgentle.Bookstore/Repository/BookRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.Bookstore.Models;
+
+namespace Webgentle.Bookstore.Repository
+{
+  public class BookRepository
+  {
+
+
+    public List<BookModel> GetAllBooks()
+    {
+      return BookStore();
+    }
+
+
+    public BookModel GetBook(int id)
+    {
+      return BookStore().Find(x => x.Id.Equals(id));
+    }
+
+    public List<BookModel> SearchBook(string name, string author)
+    {
+      return BookStore().Where(x => x.Name == name && x.Author == author).ToList();
+    }
+
+
+    private List<BookModel> BookStore()
+    {
+      return new List<BookModel>() { 
+          new BookModel{Id = 1,Name="java", Author= "Ben"},
+          new BookModel(){Id = 1,Name="c sharp", Author= "John"},
+          new BookModel(){Id = 1,Name="python", Author= "Sara"},
+          new BookModel(){Id = 1,Name="angular", Author= "Steve"},
+          new BookModel(){Id = 1,Name="react", Author= "Pam"},
+          new BookModel(){Id = 1,Name="vue", Author= "Rita"},
+      }; 
+    }
+  }
+}
