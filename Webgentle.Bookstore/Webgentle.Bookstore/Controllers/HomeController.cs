7f23514a111961efad8c6f887da1b94bc4dc93f5
PROSUN619 <prosun.chakraborty92@gmail.com>
2021-12-16 22:19:24 +0530

    part 34, viewdata

--- Webgentle.Bookstore/Webgentle.Bookstore/Controllers/HomeController.cs
+++ Webgentle.Bookstore/Webgentle.Bookstore/Controllers/HomeController.cs
@@ -3,13 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Dynamic;
+using Webgentle.Bookstore.Models;
 
 namespace Webgentle.Bookstore.Controllers
 {
   public class HomeController : Controller
   {
+    [ViewData]
+    public string MyProperty { get; set; } // we can also use view data property to display file on view page, layout page
+
     public ViewResult Index()
     {
+      //dynamic data = new ExpandoObject(); // ExpandoObject used to bind dynamic annonymus object into viewbag and display into views
+      //data.Id = 1;
+      //data.Name = "Prasun";
+
+      //ViewBag.Data = data;
+      //ViewBag.Title = "Prasun";
+      //ViewBag.Book = new BookModel() {Title = "Sample Book" }; // here we dont need to use ExpandoObject
+
+      ViewData["bookObject"] = new BookModel() {Title = "html",Author="Harry" };
+
+
+      MyProperty = "this is viewdata property";
       return View();
     }
 
