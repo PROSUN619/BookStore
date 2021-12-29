4d1bdedc2504f2ef674824f4ce2cfa8c67a19747
PROSUN619 <prosun.chakraborty92@gmail.com>
2021-12-29 18:56:44 +0530

    part 62, custom validation attribute

---
+++ Webgentle.Bookstore/Webgentle.Bookstore/Helper/CustomAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webgentle.Bookstore.Helper
+{
+  public class CustomAttribute : ValidationAttribute
+  {
+    public CustomAttribute(string text)
+    {
+      TexttoCheck = text;
+    }
+
+    public string TexttoCheck { get; set; }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+       if (value != null)
+      {
+        string val = value.ToString();
+        if (val.Contains(TexttoCheck))
+        {
+          return ValidationResult.Success;
+        }
+      }
+      return new ValidationResult(ErrorMessage ?? "Data must contain js");
+    }
+  }
+}
--- Webgentle.Bookstore/Webgentle.Bookstore/Models/BookModel.cs
+++ Webgentle.Bookstore/Webgentle.Bookstore/Models/BookModel.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Webgentle.Bookstore.Enums;
+using Webgentle.Bookstore.Helper;
 
 namespace Webgentle.Bookstore.Models
 {
   public class BookModel
   {
     public int Id { get; set; }
-    [Required]//[StringLength(maximumLength:20,MinimumLength =5)]
+    //[Required]//[StringLength(maximumLength:20,MinimumLength =5)]
+    [CustomAttribute("angular",ErrorMessage ="Error Message from model")]
     public string Title { get; set; }
     [Required]
     public string Author { get; set; }
